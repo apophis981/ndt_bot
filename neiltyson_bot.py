1e73dc505bba92e21d46b3629aab221d4224eeb9
Jeremy Hurst <apophis981@gmail.com>
2018-01-25 08:31:08 -0800

    Updated auth

--- neiltyson_bot.py
+++ neiltyson_bot.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import tweepy
-from auth import TwitterAuth
+#from auth import TwitterAuth
 from markov_chain import markov
+import imp
+
+ndt_auth = imp.load_source('TwitterAuth', '../ndt_auth.py')
 
 class TwitterAPI:
     def __init__(self):
-        auth = tweepy.OAuthHandler(TwitterAuth.consumer_key, TwitterAuth.consumer_secret)
-        auth.set_access_token(TwitterAuth.access_token, TwitterAuth.access_token_secret)
+        auth = tweepy.OAuthHandler(ndt_auth.TwitterAuth.consumer_key, ndt_auth.TwitterAuth.consumer_secret)
+        auth.set_access_token(ndt_auth.TwitterAuth.access_token, ndt_auth.TwitterAuth.access_token_secret)
         self.api = tweepy.API(auth)
 
     def tweet(self, message):
@@ -16,5 +19,5 @@ class TwitterAPI:
 if __name__ == "__main__":
     twitter = TwitterAPI()
     tweet_string = markov.tweet
-    print "Tweeting: " + tweet_string
+    print("Tweeting: " + tweet_string)
     twitter.tweet(markov.tweet)
